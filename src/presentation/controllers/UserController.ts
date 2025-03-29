abba0a0338c47f887e0b5a2e88a88f32c8e18fa8
MoisesG01 <moises.goncalves@fatec.sp.gov.br>
2025-03-29 15:58:30 -0300

    add get all users

--- src/presentation/controllers/UserController.ts
+++ src/presentation/controllers/UserController.ts
@@ -1,13 +1,17 @@
 import { Request, Response } from 'express';
 import { CreateUserUseCase } from '@application/useCases/CreateUserUseCase';
+import { GetAllUsersUseCase } from '@application/useCases/GetAllUsersUseCase';
 import { UserRepository } from '@infrastructure/repositories/UserRepository';
+import { UserDTO } from '@presentation/dtos/UserDTO';
 
 export class UserController {
     private createUserUseCase: CreateUserUseCase;
+    private getAllUsersUseCase: GetAllUsersUseCase;
     
     constructor() {
         const userRepository = new UserRepository;
         this.createUserUseCase = new CreateUserUseCase(userRepository);
+        this.getAllUsersUseCase = new GetAllUsersUseCase(userRepository);
     }
 
     async createUser(req: Request, res: Response) {
@@ -16,6 +20,21 @@ export class UserController {
             const user = await this.createUserUseCase.execute(name, email);
             
             return res.status(201).json(user);
+        } catch (error) {
+            return res.status(400).json({error: (error as Error).message })
+        }
+    }
+
+    async getAllUsers(req: Request, res: Response) {
+        try {
+            const users: UserDTO[] = await this.getAllUsersUseCase.execute();
+            
+            if(!users || users.length === 0) {
+                return res.status(404).json({message: "Nenhum usuário encontrado!"});
+            }
+
+            return res.status(200).json(users);
+
         } catch (error) {
             return res.status(400).json({error: (error as Error).message })
         }
